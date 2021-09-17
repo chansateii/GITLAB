9e87c14b3d34ff1fbb31a99244539f5a7a641e9f
chansateii <teerat.c@mail.kmutt.ac.th>
2021-09-17 11:39:10 +0700

    modified blood group.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -26,12 +26,12 @@ void main()
       printf("%s, A. Hey, you can give blood to: A, AB.\n", Name);
       printf("  You can receive blood from: A, O.\n");
     }
-    else if (strcmp(G, "B") ==0)
+    else if (strcmp(G, "B") ==1)
     {
       printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
       printf("  You can receive blood from: B, O.\n");
     }
-    else if (strcmp(G, "AB") == 0)
+    else if (strcmp(G, "AB") == 1)
     {
       printf("%s, AB. Oh my God, you can give blood only to: AB.\n", Name);
       printf("  Wow, you can receive blood from all: O, A, B, AB.\n");
